18d05ada40af985dc65f46a9d787402df8e0d8e4
TheintTheintSH <theinttheintsoehtet.edka@gmail.com>
2025-02-04 16:19:14 +0630

    input component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import { useState } from "react";
+import Input from "./components/input";
+
 function App() {
+  const [taskList, setTaskList] = useState([]);
+
+  console.log(taskList);
   return (
-    <>
-      <h2>Todo Board</h2>
-    </>
+    <div className="flex flex-col items-center justify-center py-8 gap-4">
+      <h2 className="text-xl font-semibold">Todo Board</h2>
+      <Input taskList={taskList} setTaskList={setTaskList}></Input>
+      <div>
+        {taskList.map((task, index) => (
+          <li key={index}>{task}</li>
+        ))}
+      </div>
+    </div>
   );
 }
 
