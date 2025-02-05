d6fcff92276f7209f46605d55ac63b8b9f854e5d
TheintTheintSH <theinttheintsoehtet.edka@gmail.com>
2025-02-05 15:58:23 +0630

    create board and delete function

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import { useState } from "react";
-import Input from "./components/input";
+import Input from "./components/Input";
+import Board from "./components/Board";
 
 function App() {
   const [taskList, setTaskList] = useState([]);
 
   console.log(taskList);
   return (
-    <div className="flex flex-col items-center justify-center py-8 gap-4">
-      <h2 className="text-xl font-semibold">Todo Board</h2>
-      <Input taskList={taskList} setTaskList={setTaskList}></Input>
-      <div>
+    <div className="px-12">
+      <div className="flex flex-col items-center justify-center py-8 gap-4">
+        <h2 className="text-xl font-semibold">Todo Board</h2>
+        <Input taskList={taskList} setTaskList={setTaskList}></Input>
+      </div>
+      <div className="flex flex-col gap-4 sm:grid sm:grid-cols-3 px-4 sm:px-8 md:px-10 lg:px-12">
         {taskList.map((task, index) => (
-          <li key={index}>{task}</li>
+          <Board
+            key={index}
+            index={index}
+            task={task}
+            taskList={taskList}
+            setTaskList={setTaskList}
+          />
         ))}
       </div>
     </div>
